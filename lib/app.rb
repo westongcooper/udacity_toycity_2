c9c7b8413d236c679a5759ecdfd37ac4f7b09c1b
Weston Cooper <weston@westoncooper.com>
2016-02-04 23:53:14 -0500

    create tally and sum methods; remove nested iteration

--- lib/app.rb
+++ lib/app.rb
@@ -24,21 +24,16 @@ end
 
 def output_products products_hash
   Ascii.products
+
   products_hash["items"].each do |item|
-    total_item_sales = 0
     sales_count = item['purchases'].count
+    total_item_sales = tally(item['purchases'], 'price')
+    average_sale_price = total_item_sales / sales_count
+    average_discount_percent = (1 - (average_sale_price / item['full-price'].to_f)) * 100
 
     puts "Name: #{item['title']}"
     puts "Retail price: $#{item['full-price']}"
     puts "Number of purchases: #{sales_count}"
-
-    item['purchases'].each do |sale|
-      total_item_sales += sale["price"]
-    end
-
-    average_sale_price = total_item_sales / sales_count
-    average_discount_percent = (1 - (average_sale_price / item['full-price'].to_f)) * 100
-
     puts "Total sales amount: $#{total_item_sales}"
     puts "Average price of each product: $#{average_sale_price}"
     puts "Average discount: #{average_discount_percent.round(1)}%"
@@ -48,26 +43,25 @@ end
 
 def output_brands brands
   Ascii.brands
-
   brands.each do |brand,toys|
-    brand_toys_total_cost = brand_revenue = 0
+    brand_toys_total_cost = tally(toys, 'full-price')
+    brand_revenue = sum(toys.map {|toy| tally(toy['purchases'], 'price')})
+    average_price = brand_toys_total_cost / toys.count
 
     puts "Brand: #{brand}"
     puts "Number of Toys: #{toys.count}"
-
-    toys.each do |toy|
-      brand_toys_total_cost += toy["full-price"].to_f
-      toy["purchases"].each do |sale|
-        brand_revenue += sale["price"]
-      end
-    end
-
-    average_price = brand_toys_total_cost / toys.count
-
     puts "Average price of each toy: $#{average_price.round(2)}"
     puts "Total revenue of all toys: $#{brand_revenue.round(2)}"
     puts "\n"
   end
+end
+
+def tally items, context
+  sum(items.map { |item| item[context].to_f })
+end
+
+def sum array
+  array.inject(0){|sum,x| sum + x }
 end
 
 def convert_to_brands_hash products_hash
